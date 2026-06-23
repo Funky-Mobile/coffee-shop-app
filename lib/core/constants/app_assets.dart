835d2875025cf479aa7617abfd0aa37b9bc63aad
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2026-06-23 23:26:38 +0000

    feat(core): implement home screen layout and assets
    - Add AppAssets constants for centralized asset management
    - Rename splash icon to app icon and add cart icon asset
    - Implement AppBar with menu, app logo, and cart notification badge
    - Add state

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,4 @@
+class AppAssets {
+  static String appIcon = "assets/icons/app_icon.png";
+  static String cartIcon = "assets/icons/cart_icon.png";
+}
--- lib/features/home_screen/views/widgets/home_screen.dart
+++ lib/features/home_screen/views/widgets/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_shop_app/core/constants/app_assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,13 +11,79 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Text("Home"),
+    Text("Scan / Pay"),
+    Text("Order"),
+    Text("Account"),
+    Text("Rewards")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xEEF6F2ED),
+        leading: IconButton(
+          onPressed: () {
+            ///Todo: Open drawer
+          },
+          icon: Icon(
+            Icons.menu_rounded,
+            size: 40.0,
+          )
+        ),
+        title: Image.asset(
+          AppAssets.appIcon,
+          height: 40.0,
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Stack(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    ///Todo: Open cart
+                  },
+                  icon: Image.asset(
+                    AppAssets.cartIcon,
+                    height: 32.0,
+                  )
+                ),
+
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(4.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xEE4E8D7C),
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black
+                      )
+                    ),
+                    child: Text("99")
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
       body: Center(
-        child: Text("The shimmer works!"),
+        child: _pages[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey.shade400,
         showUnselectedLabels: true,
